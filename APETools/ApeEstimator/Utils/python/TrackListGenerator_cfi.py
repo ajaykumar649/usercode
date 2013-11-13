44779a2539171453d15c864c402df220e8cf3657
Ajay Kumar <a.kumar@cern.ch>
2013-11-13 11:46:23 +0100

    Changed into separate repositry

--- APETools/ApeEstimator/Utils/python/TrackListGenerator_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-TrackListGenerator = cms.EDAnalyzer("TrackListGenerator",
-    trackSource = cms.InputTag('generalTracks'),
-)
--- APETools/ApeEstimator/Utils/python/TriggerAnalyzer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-TriggerAnalyzer = cms.EDAnalyzer('TriggerAnalyzer',
-    # Trigger results
-    triggerResults = cms.InputTag('TriggerResults','','HLT'),
-)
